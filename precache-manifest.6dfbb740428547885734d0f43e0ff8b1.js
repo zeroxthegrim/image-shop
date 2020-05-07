bd7915dc59a785053445d0c1a886c4714986f81b
Niksa <nhribljan@gmail.com>
2020-05-07 18:48:02 +0200

    Updates

---
+++ precache-manifest.6dfbb740428547885734d0f43e0ff8b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82bd0152cdd31a5923c3ab1f2df16560",
+    "url": "/image-shop/index.html"
+  },
+  {
+    "revision": "32009d2d8954c612bde8",
+    "url": "/image-shop/static/css/main.db226daa.chunk.css"
+  },
+  {
+    "revision": "295ee99b3d7336436e89",
+    "url": "/image-shop/static/js/2.df8c7d0c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/image-shop/static/js/2.df8c7d0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32009d2d8954c612bde8",
+    "url": "/image-shop/static/js/main.e540650b.chunk.js"
+  },
+  {
+    "revision": "23bd33940c6b4c624030",
+    "url": "/image-shop/static/js/runtime-main.29de9e18.js"
+  }
+]);
